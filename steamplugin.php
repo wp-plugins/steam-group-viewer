4e324fb6983a5ef5d91995a89c23d8f6b4be0898
florianchapon <florianchapon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-25 09:47:45 +0000

    D:\wordpress\steam-group-widget\trunk\steamplugin.php
    git-svn-id: https://plugins.svn.wordpress.org/steam-group-viewer/trunk@1099082 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- steamplugin.php
+++ steamplugin.php
@@ -45,78 +45,84 @@ class steamplugin
 	echo '<h1>'.get_admin_page_title().'</h1>';
 	
 	?>
-		(English)<br/><br/>
-		Here you can have a little preview of the widget with the Official CSGO Steam Group.The group adress is this one: <a href="http://steamcommunity.com/games/CSGO">http://steamcommunity.com/games/CSGO</a>.
-		We directly set the adress of the group in the Settings form with the correct width.<br/>
+		(English)
+		<div style="margin:30px 20px 30px 0px; text-align:justify;">
+		Here you can have a little preview of the widget with the Official CSGO Steam Group.The group adress is this one: 
+		<a href="http://steamcommunity.com/games/CSGO">http://steamcommunity.com/games/CSGO</a>.
+		We directly set the adress of the group in the Settings form with the correct width.
 		You can use every type of steam group adress: <br/><br/>
 		<center> <u>http://steamcommunity.com/games/"groupname"</u><br/>
 		<u> http://steamcommunity.com/groups/"groupname"</u></center>
-		<br/><br/><br/>
+		</div>
 		
 
 	
-		(French)<br/><br/>
-		Vous pouvez ici avoir un aperçu du widget avec le groupe steam officiel de Counter Strike Global Offensive. L'adresse du groupe était la suivante: <a href="http://steamcommunity.com/games/CSGO">http://steamcommunity.com/games/CSGO</a>.
-		Nous avons mis directement l'adresse du groupe et la largeur de widget de notre choix.<br/>
+		(French)
+		<div style="margin:30px 20px 30px 0px; text-align:justify;">
+		Vous pouvez ici avoir un aperçu du widget avec le groupe steam officiel de Counter Strike Global Offensive. 
+		L'adresse du groupe était la suivante: <a href="http://steamcommunity.com/games/CSGO">http://steamcommunity.com/games/CSGO</a>.
+		Nous avons mis directement l'adresse du groupe et la largeur de widget de notre choix.
 		// Vous pouvez utiliser tout les types d'adresse de groupe:<br/><br/>
 		<center><u>http://steamcommunity.com/games/"groupname"</u><br/>
 		<u>http://steamcommunity.com/groups/"groupname"</u></center>
-		<br/><br/><br/>
+		</div>
 		
 		
 		
 		<u><b>Preview test</u>:</b><br/><br/>
 		
-		<center>
-		<table>
-		<tr><td>
-			<?php echo '<img src="'.plugins_url( 'images/screenshots/screenshot%231.png', __FILE__ ).'" style="border:1px solid #CBCBCB;border-radius:5px;"/>';?>
-		</td>
-		<td>		
-		
-		<?php echo '<img src="'.plugins_url( 'images/screenshots/screenshot%232.png', __FILE__ ).'" style="border:1px solid #CBCBCB;border-radius:5px;"/>'; ?>
-			
-		</td></tr>
-		</table>
-		
-		<style>td { padding-left:40px; padding-right:40px; }</style>
-		
-		</center>
-		
-		<u><b>Author</u>:</b><br/><br/>
-		
-		
-		<div style="float:left;text-align:justify;margin-right:40px;">(English) This plugin was made by Florian Chapon, a french technical engineer specialised in chemistry. He felt in love with webdesign and webdevelopment. <br/>
-		After months of self study, he decides to start making his own web applications. <br/><br/>
-		
-		(French) Ce plugin a été crée par Florian Chapon, un ingénieur technique spécialisé en chimie. <br/>Passionné de webdesign, de webdeveloppement et après des mois d'apprentissage en autodidacte, il décide de commencer à créer 
-		ses propres applications web.
-		
-			
-		</div>
-		<div><img src="https://media.licdn.com/mpr/mpr/shrink_200_200/p/4/005/0b3/2e0/32ba2af.jpg" width="100px"/></div>
-		
-		<br/><br/>
-		<b>You can find more information about him on his official website and his social pages / Vous pouvez trouver plus d'information à son propos sur sa site officiel ainsi que sur les réseaux sociaux:</b><br/><br/>
-		
 	
-		<div style="float:left;width:100%;">
-	
-		<div style="float:left; width:auto; margin-right:40px;">
-		<a href="http://www.florian-chapon.fr">http://www.florian-chapon.fr</a>
+		<div style="width:100%; float:left;">
+		<div style="width:80%;margin:auto;">
+			<div style="float:left;width:60%;padding-right:5%;">
+			<?php echo '<img src="'.plugins_url( 'images/screenshots/screenshot%231.png', __FILE__ ).'" style="width:100%; border:1px solid #CBCBCB;border-radius:5px;"/>';?>
+			</div>
+			<div style="float:left;width:30%;padding-left:5%;">
+			<?php echo '<img src="'.plugins_url( 'images/screenshots/screenshot%232.png', __FILE__ ).'" style="width:100%; border:1px solid #CBCBCB;border-radius:5px;"/>'; ?>
+			</div>
+		</div> 
 		</div>
 		
-		<div style="float:left;margin-right:10px;">
-		<a href="https://www.facebook.com/chaponf"><img src="https://www.facebook.com/images/fb_icon_325x325.png" width="30px"></a>
-		</div>
 		
-		<div style="float:left;margin-right:10px;">
-		<a href="https://fr.linkedin.com/pub/florian-chapon/80/a6/37"><img src="http://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" width="30px"></a>
-		</div>
+		<div>
+			<u><b>Author</u>:</b><br/><br/>
+			
+			<div style="float:left;width:100%;margin-bottom:20px;">
+			<div style="float:left;width:70%;text-align:justify;padding-right:5%;">(English) This plugin was made by Florian Chapon, 
+			a french technical engineer specialised in chemistry. He felt in love with webdesign and webdevelopment.
+			After months of self study, he decides to start making his own web applications. <br/><br/>
+			
+			(French) Ce plugin a été crée par Florian Chapon, un ingénieur technique spécialisé en chimie.
+			Passionné de webdesign, de webdeveloppement et après des mois d'apprentissage en autodidacte, il décide de commencer à créer 
+			ses propres applications web.	
+			</div>
+			<div style="float:left;width:20%;padding-right:5%;">
+				<img src="https://media.licdn.com/mpr/mpr/shrink_200_200/p/4/005/0b3/2e0/32ba2af.jpg" width="100px"/>
+			</div>
+			</div>
+			
+			<br/><br/>
+			<b>You can find more information about him on his official website and his social pages / Vous pouvez trouver plus d'information à son propos sur sa site officiel ainsi que sur les réseaux sociaux:</b><br/><br/>
+			
 		
-		<div style="float:left;margin-right:10px;">
-		<a href="https://twitter.com/florianchapon"><img src="https://g.twimg.com/Twitter_logo_blue.png" width="30px"></a>
-		</div>
+			<div style="float:left;width:100%;">
+		
+			<div style="float:left; width:auto; margin-right:40px;">
+			<a href="http://www.florian-chapon.fr">http://www.florian-chapon.fr</a>
+			</div>
+			
+			<div style="float:left;margin-right:10px;">
+			<a href="https://www.facebook.com/chaponf"><img src="https://www.facebook.com/images/fb_icon_325x325.png" width="30px"></a>
+			</div>
+			
+			<div style="float:left;margin-right:10px;">
+			<a href="https://fr.linkedin.com/pub/florian-chapon/80/a6/37"><img src="http://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" width="30px"></a>
+			</div>
+			
+			<div style="float:left;margin-right:10px;">
+			<a href="https://twitter.com/florianchapon"><img src="https://g.twimg.com/Twitter_logo_blue.png" width="30px"></a>
+			</div>
+			</div>
 		</div>
 		
 		
