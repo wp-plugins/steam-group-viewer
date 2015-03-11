b1f210c7f4d2b09a30320e530855b50f79097b3c
florianchapon <florianchapon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-11 10:06:22 +0000

    D:\wordpress\steam-group-widget\tags\2.1
    git-svn-id: https://plugins.svn.wordpress.org/steam-group-viewer/tags/2.1@1110207 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ scripts/widget.php
@@ -0,0 +1,161 @@
+<?php 
+
+		include_once($_SERVER['DOCUMENT_ROOT'].'/wp-load.php' );
+		global $wpdb;
+		$test = $wpdb->get_results("SELECT id, groupadress, groupwidth FROM {$wpdb->prefix}steam_group_widget ORDER BY id DESC LIMIT 1");
+
+		foreach ($test as $_test) {
+		
+		$groupadressiframe = $_test->groupadress;
+		$groupwidthiframe = $_test->groupwidth;
+			
+		}
+		
+		
+
+		
+	/* $url = "http://steamcommunity.com/groups/"; */
+	$url = $groupadressiframe."/memberslistxml/?xml=1";
+	$width = $groupwidthiframe-((6/100)*$groupwidthiframe);
+	$padding = (3/100)*$groupwidthiframe;
+	$imagewidth =((18/100)*$width);
+
+
+
+
+	$xml = simplexml_load_file($url);
+
+	/* $avatar = $xml->groupDetails->avatarFull;*/
+	$avatar = $xml->groupDetails->avatarIcon;
+	$groupname = $xml->groupDetails->groupName;
+	$member = $xml->groupDetails->memberCount;
+	$online = $xml->groupDetails->membersOnline;
+	$ingame = $xml->groupDetails->membersInGame;
+	$inchat = $xml->groupDetails->membersInChat;
+	$groupurl = $xml->groupDetails->groupURL;
+
+
+
+	?>
+	 
+
+	<!-- Affichage widget -->
+	<div id ="widget" style="width:auto; padding:<? echo $padding."px"; ?>; border-radius: 5px;">
+				
+				<!-- Groupe -->
+
+				<div style="height:auto;width:100%;">
+					<div style="display: inline-block; vertical-align: middle; width:18%;"><img src="<? echo $avatar;?>" width="<? echo $imagewidth; ?>" style="border-radius:100%;"/></div>
+					<div style="display: inline-block; vertical-align: middle;width:77%;height:<? echo $imagewidth; ?>;font-size:1.2em;"><font color="#3b5998"><b><span><? echo $groupname;?></span></b></font></div>
+				</div>
+				
+				<div id="separator"></div>
+				
+				<div>
+				<!-- Stats -->
+				<ul>
+					<li id="stats" style="color:#FF6600;">
+						<div id="statsnumber"><? echo $member; ?></div>
+						<div>MEMBERS</div>
+					</li>
+					<li id="stats" style="color:#62a7e3;">
+						<div id="statsnumber"><? echo $online; ?></div>
+						<div>ONLINE</div>
+					</li>
+					<li id="stats" style="color:#8bc53f;">
+						<div id="statsnumber"><? echo $ingame; ?></div>
+						<div>INGAME</div>
+					</li>
+					<li id="stats" style="color:#9a9a9a;">
+						<div id="statsnumber"><? echo $inchat; ?></div>
+						<div>IN CHAT</div>
+					</li>
+				</ul>
+				</div>
+				
+				<div id="separator"></div>	
+				
+				<!-- Boutton rejoindre -->
+				<table width="100%">
+					<tr>
+						<td valign="middle" width="65%" style="border:0;color:#000000;"><u>Join US !</u>
+						</td>
+						<td align="right" valign="middle" width="35%" style="padding:0;border:0;">
+							<div style="width:100%; height: 22px;border:none; border-radius:2px; background: #a4d007; background: -moz-linear-gradient(top, #a4d007 5%, #6b8805 95%);
+							background: -webkit-gradient(linear, left top, left bottom, color-stop(5%,#a4d007), color-stop(95%,#6b8805));
+							background: -webkit-linear-gradient(top, #a4d007 5%,#6b8805 95%);
+							background: -o-linear-gradient(top, #a4d007 5%,#6b8805 95%);
+							background: -ms-linear-gradient(top, #a4d007 5%,#6b8805 95%);
+							background: linear-gradient(to bottom, #a4d007 5%,#6b8805 95%);
+							text-align:center;">
+							<a href="http://steamcommunity.com/groups/<?php echo $groupurl;?>" target="_BLANCK" style="text-decoration:none; display:block; color:white;width:100%; height:22px;line-height:22px;">Group</a></div></td>
+					</tr>
+				</table>
+			
+
+			
+				<!-- Mozaique de membres -->			
+				<?php 
+
+				$total = $xml->groupDetails->memberCount;
+
+				// Nombre aléatoire pour profil
+				$rand = rand(0, 1000);
+				 
+
+				// de base 0 et 9 
+				echo "<ul style='text-align:center;'>";
+				
+					for ($i=0;$i<=9; $i++)
+					{
+					$rand2 = $rand + $i;
+					$profilapi= "http://steamcommunity.com/profiles/";
+					$profilapi .= $xml->members->steamID64[$i];
+					$profilapi .= "/?xml=1";
+					
+
+					
+					$xmlprofil = simplexml_load_file($profilapi); 
+					
+					
+					echo "<li id='profil' style='";
+					
+					if ($xmlprofil->onlineState == "online")
+					{
+					
+					echo "border-color:#7bafd6;'>";
+					
+					}
+					
+					
+					elseif ($xmlprofil->onlineState == "in-game")
+					{
+					echo "border-color:#9bc861;'>";
+					}
+					
+					else
+					
+					{
+					echo "border-color:#545454;'>";
+					
+					}
+					echo "<a href='http://steamcommunity.com/profiles/".$xml->members->steamID64[$i]."' target='_blank' title='".$xmlprofil->steamID."'><img src='".$xmlprofil->avatarMedium."' id='profileimg' width='100%'/></a></li>";
+
+					
+					}
+					echo "</ul>";
+				
+				?></div>
+
+				
+<!-- Not use right now -->
+	<script type="text/javascript">
+
+		$("body").each(function(i, obj) {
+			newSize = ((<?php echo $groupwidthiframe; ?>)/300)*14;
+			$(obj).css("font-size", newSize + "px");
+		});
+
+	</script>
+		
+	
